65017ae110a5cb1dfffc94ef030ccbb2603941a3
SRC0747 <RjSrc@0747>
2021-11-11 20:40:13 +0530

    Added: Failed testcase and code for parking car and catch the flight.

--- src/test/java/com/bridgelabz/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/ParkingLotTest.java
@@ -1,7 +1,15 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotTest {
 
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assertions.assertTrue(isParked);
+    }
 }
